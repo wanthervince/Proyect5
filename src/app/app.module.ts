3c76d420f66c59a9e13b60d979bc748f0f30aea4
WanTher-VinCe <lonely.wolf.1594@gmail.com>
2021-09-04 10:32:07 -0500

    push github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TableComponent } from './dashboard/table/table.component';
+import { ListComponent } from './dashboard/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    TableComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
